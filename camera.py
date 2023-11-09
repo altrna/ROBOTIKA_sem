db055117c6202a0526f17127372f33fee55a491b
altrna <altrna@seznam.cz>
2023-11-09 18:16:04 +0100

    aruco detect

--- camera.py
+++ camera.py
@@ -11,6 +11,10 @@ camera.connect(bus.getCameraFromIndex(0))
 
 # Start capture
 camera.startCapture()
+
+aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+params = cv2.aruco.DetectorParameters_create()
+
 i = 0
 while True:
   # Retrieve image from camara in PyCapture2.Image format
@@ -21,6 +25,10 @@ while True:
 
   # Convert image to Numpy array
   rgb_cv_image = np.array(image.getData(), dtype="uint8").reshape((image.getRows(), image.getCols(), 3))
+
+  corners, ids, rejected_im_points = cv2.aruco.detectMarkers(rgb_cv_image, aruco_dict,parameters = params)
+  rgb_cv_image = cv2.aruco.drawDetectedMarkers(rgb_cv_image, corners = corners, ids=ids, borderColor=(0, 255, 255))
+  print(corners, "\n")
 
   # Convert RGB image to BGR image to be shown by OpenCV
   bgr_cv_image = cv2.cvtColor(rgb_cv_image, cv2.COLOR_RGB2BGR)
