9dc50ff5fe6750913045b9571438ff5448105bf1
Minh Hoang Tran <tranmh2002@gmail.com>
2023-12-10 11:48:45 +0100

    new cam_r/t

--- Hand2Eye_calib.py
+++ Hand2Eye_calib.py
@@ -1,38 +1,43 @@
 import cv2
 from core.so3 import *
-
-"""
-[450 -10 550   0   0   0]
-Robot:  [  4.49998826e+02  -1.00424940e+01   5.50018150e+02  -4.90701886e-03
-  -2.20060224e-03  -6.01824994e-03] [-185161   18656   63938  108305    9532 -107155]
-Camera:  [[[ 0.01389302  0.07296657  0.68056817]]] [[[-2.13977189  2.15591813  0.09443021]]]
-"""
+import glob
 
 
-# X, Y = cv2.calibrateRobotWorldHandEye()
 
-# t2c_rot = SO3.from_euler_angles([-2.13977189, 2.15591813, 0.09443021], "xyz")
+DIST_MATRIX = np.array([[  2.31616457e+03  , 0.00000000e+00 ,  6.31242571e+02],
+ 	[  0.00000000e+00  , 2.34163983e+03  , 2.40442592e+02],
+ 	[  0.00000000e+00 ,  0.00000000e+00  , 1.00000000e+00]])
+CAM_MATRIX = np.array([  1.31600325e-01 , -2.13500794e+00 , -3.13652860e-02,   1.69360473e-03,
+    9.34997291e+00])
 
-# t2c_rot = np.asmatrix(t2c_rot.rot)
-# b2r_rot = np.asmatrix(np.eye(3))
-
-<<<<<<< HEAD:tmp.py
-pos_r = [SO3.from_euler_angles(np.rad2deg(i.flatten()), 'xyz').rot for i in np.load("posr.npz", allow_pickle=True)["arr_0"]]
-pos_t = [i.flatten() for i in np.load("post.npz", allow_pickle=True)["arr_0"]]
-cam_r = [SO3.from_euler_angles(i.flatten(), 'xyz').rot for i in np.load("cam_r_vec.npz", allow_pickle=True)["arr_0"]]
-cam_t = [1e3 * i.flatten() for i in np.load("cam_t_vec.npz", allow_pickle=True)["arr_0"]]
-=======
 pos_r = [
     (SO3.rz(np.deg2rad(i.flatten())[0]) * SO3.ry(np.deg2rad(i.flatten())[1]) * SO3.rx(np.deg2rad(i.flatten())[2])).rot
     for i in np.load("posr.npz", allow_pickle=True)["arr_0"]
 ]
 pos_t = [i.flatten() for i in np.load("post.npz", allow_pickle=True)["arr_0"]]
->>>>>>> refs/remotes/origin/main:Hand2Eye_calib.py
 
 cam_t = [i.flatten() for i in np.load("cam_t_vec.npz", allow_pickle=True)["arr_0"]]
-
-# pos_r = [np.deg2rad(i.flatten()) for i in np.load("posr.npz", allow_pickle=True)["arr_0"]]
 cam_r = [i.flatten() for i in np.load("cam_r_vec.npz", allow_pickle=True)["arr_0"]]
+
+images = glob.glob("./images/hand2eye/*.png")
+
+# for filename in images:
+#     coord = filename.split("/")[3]
+#     coord = coord.split("_")
+#     image = cv2.imread(filename) 
+#     aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+#     params = cv2.aruco.DetectorParameters_create()
+#     rgb_cv_image = np.array(image.getData(), dtype="uint8").reshape((image.getRows(), image.getCols(), 3))
+#     gray_cv_image = cv2.cvtColor(rgb_cv_image, cv2.COLOR_BGR2GRAY)
+#     ret, thresh = cv2.threshold(gray_cv_image, 80, 255, 0)
+
+#     corners, ids, rejected_im_points = cv2.aruco.detectMarkers(thresh, aruco_dict,parameters = params)
+#     rgb_cv_image = cv2.aruco.drawDetectedMarkers(rgb_cv_image, corners = corners, ids=ids, borderColor=(0, 255, 255))
+#     cam_r, cam_t= cv2.aruco.estimatePoseSingleMarkers(corners, 0.039, CAM_MATRIX, DIST_MATRIX)
+    
+
+
+
 
 for i in range(len(cam_r)):
     cam_r[i] = SO3.exp(cam_r[i]).rot
---
+++ main3.py
@@ -0,0 +1,132 @@
+import sys
+import numpy as np
+sys.path.append("/bluebot/data/Pyrocon")
+from CRS_commander import Commander
+from robCRSgripper import robCRSgripper
+from robCRSdkt import robCRSdkt
+from robCRSikt import robCRSikt
+from robotCRS import robCRS97
+from core.se3 import *
+from core.so3 import *
+import PyCapture2
+import cv2
+
+CAM_MATRIX = np.array([[  2.10813831e+03, 0.00000000e+00,   6.49912597e+02],
+        [  0.00000000e+00,   2.11337088e+03,   3.45497271e+02],
+        [  0.00000000e+00,   0.00000000e+00,   1.00000000e+00]])
+DIST_MATRIX = np.array([ -6.60263309e-02, -4.23421181e-01,  -1.32225502e-02,   1.26508906e-03, 2.59213104e+00])
+
+class Box:
+    def __init__(self, SE3, box_id, cubes_id):
+        self.SE3 = SE3
+        self.id = box_id
+        self.cubes_id = cubes_id
+
+class Aruco:
+    def __init__(self, SE3, aruco_id):
+        self.SE3 = SE3
+        self.id = aruco_id
+        self.layer = self.get_layer()	
+
+    def get_layer(self):
+        return np.clip((self.SE3.translation[2]-5)//50, -1, 5)
+
+    def is_grabbable(self, all_arucos):
+        ...
+        self.grabbable = ...
+    def __repr__(self):
+        return "Layer: " + str(self.layer) + "\nSE3: " + self.SE3.__repr__() + "\nID: " + str(self.id)
+
+def get_arucos_pose(camera, camera_matrix, distortion_matrix, base2cam):
+    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+    params = cv2.aruco.DetectorParameters_create()
+    image = camera.retrieveBuffer()
+    image = image.convert(PyCapture2.PIXEL_FORMAT.RGB8)
+
+
+    rgb_cv_image = np.array(image.getData(), dtype="uint8").reshape((image.getRows(), image.getCols(), 3))
+    gray_cv_image = cv2.cvtColor(rgb_cv_image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray_cv_image, 80, 255, 0)
+
+    corners, ids, rejected_im_points = cv2.aruco.detectMarkers(thresh, aruco_dict,parameters = params)
+    r_vec, t_vec = cv2.aruco.estimatePoseSingleMarkers(corners, 39, camera_matrix, distortion_matrix)
+    l = np.shape(r_vec)[0]
+    arucos = []
+    for i in range(l):
+        cam2aruco = SE3(t_vec[i].flatten(), SO3.exp(r_vec[i].flatten()))
+        base2aruco = base2cam*cam2aruco
+        arucos.append(Aruco(base2aruco, ids[i]))
+    return arucos
+
+def move_to_pos(cmd, des_pos):
+    try:
+        irc = cmd.find_closest_ikt(des_pos)
+        cmd.wait_ready()
+        cmd.coordmv(irc)
+        cmd.wait_ready()
+        return True
+    except:
+        irc = None
+        print("ERROR: No IKT!")
+        return False
+
+def release(cmd, val=-1):
+    robCRSgripper(cmd, val)
+
+def pick_up(cmd, cube_se3):
+    release(cmd, val=-1.5)
+    cube_se3.translation[0:2] += [20,-8]
+    cube_se3.translation[2] = 100
+    yaw = np.arctan2(cube_se3.rotation.rot[1,0], cube_se3.rotation.rot[0,0])
+    des_pos = [*cube_se3.translation, np.rad2deg(yaw), 90, 0]
+    cont = move_to_pos(cmd, des_pos)
+    if not cont:
+        return
+    
+    des_pos[2] = 50
+    cont = move_to_pos(cmd, des_pos)
+    if not cont:
+        return
+    robCRSgripper(cmd, 0.1)
+    cmd.wait_gripper_ready()
+
+    des_pos[2] = 200
+    cont = move_to_pos(cmd, des_pos)
+    if not cont:
+        return
+    
+def aruco_init(cmd, camera, camera_matrix, distortion_matrix, base2cam):
+    move_to_pos(cmd, [0,0,0,0,0,0])
+    arucos = get_arucos_pose(camera, camera_matrix, distortion_matrix, base2cam)
+    
+
+if __name__ == "__main__":
+    robot = robCRS97()
+    cmd = Commander(robot)
+    cmd.open_comm("/dev/ttyUSB0", speed=19200)
+    cmd.init(hard_home=False)
+    #cmd.hard_home()
+    cmd.soft_home()	
+    robCRSgripper(cmd, 0)
+    bus = PyCapture2.BusManager()
+    camera = PyCapture2.Camera()
+
+    camera.connect(bus.getCameraFromIndex(0))
+    camera.startCapture()
+
+    camr = np.load("Cam_R.npz")["arr_0"]
+    camt = np.load("Cam_T.npz")["arr_0"].flatten()
+    base2cam = SE3(camt, SO3(camr))
+
+    cp = get_arucos_pose(camera, CAM_MATRIX, DIST_MATRIX, base2cam)
+    for i in range(len(cp)):
+        #print(cp[i])
+        print(cp[i])
+        #pick_up(cmd, cp[i])
+        #move_to_pos(cmd, [0, -400, 150, 0, 90, 0])
+        release(cmd)
+    
+    
+    
+
+
